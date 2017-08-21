8b161ae1164b1eef4907a1ca0b37e96c36f1f4a2
Mike <sirikecarter@gmail.com>
2017-08-20 19:01:26 -0700

    tests

---
+++ test/Splitter.js
@@ -0,0 +1,33 @@
+var Splitter = artifacts.require("./Splitter.sol");
+
+contract('Splitter', function(accounts) {
+
+	var contract;	
+	address1 = accounts[0];
+	address2 = accounts[1];
+	address3 = accounts[2];
+	owner = accounts[0];
+
+
+	beforeEach(function() {
+		return Splitter.new(address1,address2,address3, {from:owner})
+		.then(function(instance) {
+			contract = instance;
+
+		});
+	});
+
+	it("should say Hello", function() {
+		assert.strictEqual(true,true,"Something is wrong.");
+	});
+
+	it("should be owned by owner", function() {
+		return contract.alice({from:owner})
+		.then(function(_owner) {
+	assert.strictEqual(owner,_owner,"Contract is not owned by Owner.");
+
+		});
+		
+	});
+
+});
